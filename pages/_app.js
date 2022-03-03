6bc83de51815df6915c9d0159abe0e09467d92d1
Aubrey Barnes <aubrey.hbarnes@gmail.com>
2022-03-03 08:05:53 -0600

    never added the actual project files after commit the first time

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,11 @@
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return( 
+  <div className='bg-gradient-to-l from-green-200 via-indigo-200 to-purple-200'>
+    <Component {...pageProps} />
+  </div>
+  );
 }
 
 export default MyApp
